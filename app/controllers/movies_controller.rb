0ed954ef79f25a1604209ec5fad6d615b174ad84
Christopher Duerkes <christopher.duerkes@gmail.com>
2018-04-29 01:54:10 +0000

    Add logo, devise, and User model.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,4 +6,15 @@ class MoviesController < ApplicationController
   def new
     @movie = Movie.new
   end
+
+  def create
+    Movie.create(movie_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :summary)
+  end
 end
